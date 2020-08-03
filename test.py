d6f90f549228581d0f9c5bbf3cbfb79191facfe4
Karim botros <kim0.swimmer@gmail.com>
2020-08-03 15:23:27 +0200

    Update test.py

--- test.py
+++ test.py
@@ -1,2 +1,8 @@
 import os
-print('hello Universe')
+
+
+if __name__ == "__main__":
+    readme = root / "README.md"
+    readme_contents = readme.open().read()
+    
+    readme.open("w").write('hii')
