0e085672ea87bb15c3a71d716f2340b9aa958f4b
Karim botros <kim0.swimmer@gmail.com>
2020-08-03 17:40:39 +0200

    add inspirational qoutes

--- test.py
+++ test.py
@@ -1,6 +1,8 @@
 import pathlib
 import re
 import os
+import bs4
+from bs4 import BeautifulSoup
 import requests
 
 root = pathlib.Path(__file__).parent.resolve()
@@ -8,5 +10,13 @@ root = pathlib.Path(__file__).parent.resolve()
 if __name__ == "__main__":
     readme = root / "README.md"
     readme_contents = readme.open().read()
-    r = requests.get('https://quotes.rest/quote/random?language=en&limit=1')
-    readme.open("w").write(r.text)
+    url = "https://quotes15.p.rapidapi.com/quotes/random/"
+    querystring = {"language_code":"en"}
+    headers = {
+        'x-rapidapi-host': "quotes15.p.rapidapi.com",
+        'x-rapidapi-key': "8f79eb00b6msh6295f7072540bd7p1d294ajsn323ad8c01598"
+    }
+    res = requests.request("GET", url, headers=headers, params=querystring)
+    data = res.json()
+    #print(data['content'])
+    readme.open("w").write(data['content']+' by '+data['originator']['name'])
