538f4953585dae3215d8025a50a2802e229b18a7
Karim botros <kim0.swimmer@gmail.com>
2020-08-03 16:18:07 +0200

    Update test.py

--- test.py
+++ test.py
@@ -9,4 +9,4 @@ if __name__ == "__main__":
     readme = root / "README.md"
     readme_contents = readme.open().read()
     r = requests.get('https://quotes.rest/quote/random?language=en&limit=1')
-    readme.open("w").write(r)
+    readme.open("w").write(r.text)
